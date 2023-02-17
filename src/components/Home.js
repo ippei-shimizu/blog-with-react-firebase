a7ae58f154afdedd65db8ad82c165ef59f096f15
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-02-17 09:24:02 +0900

    firebaseのデータを取得してconsole.logで表示したまで

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { db } from "../firebase";
+import "./Home.css";
 
 const Home = () => {
-  return <div>home</div>;
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          postTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainer
+        </div>
+        <div className="nameAndDeleteButton">
+          <h2>@ippei</h2>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
