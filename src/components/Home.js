0f2d019e2d9309107e18c63ea18bc6c2085d97b5
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-02-17 20:43:54 +0900

    データベースから投稿記事情報をHTMLとして表示させた

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,35 @@
 import { collection, getDocs } from "firebase/firestore";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import "./Home.css";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   }, []);
 
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          postTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainerstTextContainer
-        </div>
-        <div className="nameAndDeleteButton">
-          <h2>@ippei</h2>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h2>@{post.author.username}</h2>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
